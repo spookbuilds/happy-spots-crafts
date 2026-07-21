2536605dd276688e46602ba764f6d8029c60b3e9
spookbuilds <rosiesouter1993@gmail.com>
2026-07-21 17:22:23 +0100

    added photos to vehicles and pets and updated CSS added a lightbox for each

--- gallery.js
+++ gallery.js
@@ -13,3 +13,121 @@ sliders.forEach(slider => {
     });
 
 });
+
+
+
+
+
+// Lightbox - 
+
+const galleryImages = document.querySelectorAll(".gallery-item img");
+
+const lightbox = document.querySelector(".lightbox");
+const lightboxImage = document.querySelector(".lightbox-image");
+
+const close = document.querySelector(".close");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+
+let currentIndex = 0;
+
+
+// Open lightbox
+galleryImages.forEach((image, index) => {
+
+    image.addEventListener("click", () => {
+
+        currentIndex = index;
+
+        showImage();
+
+        lightbox.style.display = "flex";
+
+    });
+
+});
+
+
+// Show selected image
+function showImage(){
+
+    lightboxImage.src = galleryImages[currentIndex].src;
+
+}
+
+
+// Next image
+function nextImage(){
+
+    currentIndex++;
+
+    if(currentIndex >= galleryImages.length){
+        currentIndex = 0;
+    }
+
+    showImage();
+
+}
+
+
+// Previous image
+function previousImage(){
+
+    currentIndex--;
+
+    if(currentIndex < 0){
+        currentIndex = galleryImages.length - 1;
+    }
+
+    showImage();
+
+}
+
+
+// Buttons
+next.addEventListener("click", nextImage);
+
+prev.addEventListener("click", previousImage);
+
+
+// Close button
+close.addEventListener("click", () => {
+
+    lightbox.style.display="none";
+
+});
+
+
+// Click outside image closes
+lightbox.addEventListener("click", (e)=>{
+
+    if(e.target === lightbox){
+        lightbox.style.display="none";
+    }
+
+});
+
+
+// Keyboard controls
+document.addEventListener("keydown", (e)=>{
+
+    if(lightbox.style.display === "flex"){
+
+        if(e.key === "ArrowRight"){
+            nextImage();
+        }
+
+
+        if(e.key === "ArrowLeft"){
+            previousImage();
+        }
+
+
+        if(e.key === "Escape"){
+            lightbox.style.display="none";
+        }
+
+    }
+
+});
